f49ab1260ed1b6df31be8c44d432e65c75cbb226
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 19:51:27 +0200

    add:client/login.jsx: The handle submit function has been created

--- client/src/routes/login/login.jsx
+++ client/src/routes/login/login.jsx
@@ -1,23 +1,67 @@
-import "./login.scss";
-import { Link } from "react-router-dom";
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+
+import './login.scss';
+import apiRequest from '../../lib/apiRequest';
 
 function Login() {
-  return (
-    <div className="login">
-      <div className="formContainer">
-        <form>
-          <h1>Welcome back</h1>
-          <input name="username" type="text" placeholder="Username" />
-          <input name="password" type="password" placeholder="Password" />
-          <button>Login</button>
-          <Link to="/register">{"Don't"} you have an account?</Link>
-        </form>
+   const [error, setError] = useState('');
+   const [isLoading, setIsLoading] = useState(false);
+   const navigate = useNavigate();
+
+   const handleSubmit = async (e) => {
+      setError('');
+      setIsLoading(true);
+      e.preventDefault();
+      const formData = new FormData(e.target);
+
+      const username = formData.get('username');
+      const password = formData.get('password');
+
+      try {
+         const res = await apiRequest.post('/auth/login', {
+            username,
+            password,
+         });
+
+         localStorage.setItem('user', JSON.stringify(res.data));
+
+         navigate('/');
+      } catch (error) {
+         setError(error.response.data.message);
+      } finally {
+         setIsLoading(false);
+      }
+   };
+   return (
+      <div className="login">
+         <div className="formContainer">
+            <form onSubmit={handleSubmit}>
+               <h1>Welcome back</h1>
+               <input
+                  name="username"
+                  type="text"
+                  placeholder="Username"
+                  required
+                  minLength={3}
+                  maxLength={20}
+               />
+               <input
+                  name="password"
+                  type="password"
+                  placeholder="Password"
+                  required
+               />
+               {error && <span>{error}</span>}
+               <button disabled={isLoading}>Login</button>
+               <Link to="/register">{"Don't"} you have an account?</Link>
+            </form>
+         </div>
+         <div className="imgContainer">
+            <img src="/bg.png" alt="" />
+         </div>
       </div>
-      <div className="imgContainer">
-        <img src="/bg.png" alt="" />
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Login;
