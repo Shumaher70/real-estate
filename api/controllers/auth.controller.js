cc95818f5d4875922f51b1e5c8c62734071c2bd5
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 14:14:51 +0200

    create:api/auth.controller.js: the initial register function has been created and checked

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt';
+
+export const register = async (req, res) => {
+   const { username, email, password } = req.body;
+
+   const hashedPassword = await bcrypt.hash(password, 10);
+
+   console.log(hashedPassword);
+};
+
+export const login = (req, res) => {};
+
+export const logout = (req, res) => {};
