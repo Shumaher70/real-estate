6fd6c44d3cb0dc2f1ce5047439f84cf68c2cde0d
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 15:18:33 +0200

    add:api/auth.controller.js: 01.The logic in register function for created new user has been added 02.The logic in login function for checking existing user has been added

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,13 +1,57 @@
 import bcrypt from 'bcrypt';
+import prisma from '../lib/prisma.js';
 
 export const register = async (req, res) => {
    const { username, email, password } = req.body;
 
-   const hashedPassword = await bcrypt.hash(password, 10);
+   try {
+      const hashedPassword = await bcrypt.hash(password, 10);
 
-   console.log(hashedPassword);
+      if (!username) {
+         return res.status(404).json({ message: 'Username is not exist' });
+      }
+
+      if (!email) {
+         return res.status(404).json({ message: 'Email is not exist' });
+      }
+
+      await prisma.user.create({
+         data: {
+            username,
+            email,
+            password: hashedPassword,
+         },
+      });
+
+      res.status(201).json({ message: 'User created successfully' });
+   } catch (error) {
+      console.log('[register]', error);
+      res.status(500).json({ message: 'Failed to create user' });
+   }
 };
 
-export const login = (req, res) => {};
+export const login = async (req, res) => {
+   const { username, password } = req.body;
+   try {
+      const user = await prisma.user.findUnique({
+         where: {
+            username,
+         },
+      });
 
-export const logout = (req, res) => {};
+      if (!user) {
+         return res.status(401).json({ message: 'Invalid Credentials' });
+      }
+
+      const isPasswordValid = await bcrypt.compare(password, user.password);
+
+      if (!isPasswordValid) {
+         return res.status(401).json({ message: 'Invalid Credentials' });
+      }
+   } catch (error) {
+      console.log('[login]', error);
+      res.status(500).json({ message: 'Failed to login' });
+   }
+};
+
+export const logout = async (req, res) => {};
