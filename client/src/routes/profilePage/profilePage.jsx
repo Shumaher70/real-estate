b15546f14234e3370a2024d3937433c6a346fc37
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 19:51:52 +0200

    add:profilePage: The handleLogout function has been created

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,49 +1,64 @@
-import Chat from "../../components/chat/Chat";
-import List from "../../components/list/List";
-import "./profilePage.scss";
+import apiRequest from '../../lib/apiRequest';
+import Chat from '../../components/chat/Chat';
+import List from '../../components/list/List';
+import './profilePage.scss';
+import { useNavigate } from 'react-router-dom';
 
 function ProfilePage() {
-  return (
-    <div className="profilePage">
-      <div className="details">
-        <div className="wrapper">
-          <div className="title">
-            <h1>User Information</h1>
-            <button>Update Profile</button>
-          </div>
-          <div className="info">
-            <span>
-              Avatar:
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
-            </span>
-            <span>
-              Username: <b>John Doe</b>
-            </span>
-            <span>
-              E-mail: <b>john@gmail.com</b>
-            </span>
-          </div>
-          <div className="title">
-            <h1>My List</h1>
-            <button>Create New Post</button>
-          </div>
-          <List />
-          <div className="title">
-            <h1>Saved List</h1>
-          </div>
-          <List />
-        </div>
+   const navigate = useNavigate();
+
+   const handleLogout = async () => {
+      try {
+         await apiRequest.post('/auth/logout');
+         localStorage.removeItem('user');
+         navigate('/');
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   return (
+      <div className="profilePage">
+         <div className="details">
+            <div className="wrapper">
+               <div className="title">
+                  <h1>User Information</h1>
+                  <button>Update Profile</button>
+               </div>
+               <div className="info">
+                  <span>
+                     Avatar:
+                     <img
+                        src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt=""
+                     />
+                  </span>
+                  <span>
+                     Username: <b>John Doe</b>
+                  </span>
+                  <span>
+                     E-mail: <b>john@gmail.com</b>
+                  </span>
+                  <button onClick={handleLogout}>Logout</button>
+               </div>
+               <div className="title">
+                  <h1>My List</h1>
+                  <button>Create New Post</button>
+               </div>
+               <List />
+               <div className="title">
+                  <h1>Saved List</h1>
+               </div>
+               <List />
+            </div>
+         </div>
+         <div className="chatContainer">
+            <div className="wrapper">
+               <Chat />
+            </div>
+         </div>
       </div>
-      <div className="chatContainer">
-        <div className="wrapper">
-          <Chat/>
-        </div>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default ProfilePage;
