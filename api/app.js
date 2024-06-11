84f8727767a8fca7862e2770b96e4f76b43b6c8e
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 19:41:40 +0200

    add:app.js: The middleware for the cors and cookieParser have been added

--- api/app.js
+++ api/app.js
@@ -1,10 +1,20 @@
 import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 import authRoute from './routes/auth.route.js';
 import postRoute from './routes/post.route.js';
 
 const app = express();
 
+app.use(
+   cors({
+      origin: process.env.CLIENT_URL,
+      credentials: true,
+   })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/posts', postRoute);
 app.use('/api/auth', authRoute);
