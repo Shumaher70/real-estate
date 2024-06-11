8bc25f401381cd4e83ae208e64e8baaf4ea47402
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-11 14:12:40 +0200

    add:api/app.js: The middleware for post,auth and express.json() has been added

--- api/app.js
+++ api/app.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import authRoute from './routes/auth.route.js';
+import postRoute from './routes/post.route.js';
 
 const app = express();
+
+app.use(express.json());
+
+app.use('/api/posts', postRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(8800, () => {
    console.log('server is running');
