ae7004bbf8183437461c017f619e91c06eb77799
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-12 14:35:50 +0200

    add:client/layout.jsx: The additional layout RequiredAuth for protection route has been created

--- client/src/routes/layout/layout.jsx
+++ client/src/routes/layout/layout.jsx
@@ -1,18 +1,36 @@
-import "./layout.scss";
-import Navbar from "../../components/navbar/Navbar"
-import { Outlet } from "react-router-dom";
+import { useContext } from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
 
-function Layout() {
-  return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
+import './layout.scss';
+import Navbar from '../../components/navbar/Navbar';
+import { AuthContext } from '../../context/AuthContext';
+
+export function Layout() {
+   return (
+      <div className="layout">
+         <div className="navbar">
+            <Navbar />
+         </div>
+         <div className="content">
+            <Outlet />
+         </div>
       </div>
-      <div className="content">
-        <Outlet/>
-      </div>
-    </div>
-  );
+   );
 }
 
-export default Layout;
+export function RequireAuth() {
+   const { currentUser } = useContext(AuthContext);
+
+   if (!currentUser) return <Navigate to="/login" />;
+
+   return (
+      <div className="layout">
+         <div className="navbar">
+            <Navbar />
+         </div>
+         <div className="content">
+            <Outlet />
+         </div>
+      </div>
+   );
+}
