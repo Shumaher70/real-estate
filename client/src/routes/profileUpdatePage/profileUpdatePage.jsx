a338d0e6d03be9930a5402395a98f1e6a360404f
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-12 18:34:53 +0200

    add:client/profileUpdatePage.jsx: The logic for updating profile has been added

--- client/src/routes/profileUpdatePage/profileUpdatePage.jsx
+++ client/src/routes/profileUpdatePage/profileUpdatePage.jsx
@@ -1,39 +1,77 @@
-import "./profileUpdatePage.scss";
+import { useContext, useState } from 'react';
+import './profileUpdatePage.scss';
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
+import { useNavigate } from 'react-router-dom';
 
 function ProfileUpdatePage() {
-  return (
-    <div className="profileUpdatePage">
-      <div className="formContainer">
-        <form>
-          <h1>Update Profile</h1>
-          <div className="item">
-            <label htmlFor="username">Username</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
+   const { currentUser, updateUser } = useContext(AuthContext);
+   const [error, setError] = useState('');
+   const [avatar, setAvatar] = useState([]);
+
+   const navigate = useNavigate();
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+
+      const { username, email, password } = Object.fromEntries(formData);
+
+      try {
+         const res = await apiRequest.put(`/users/${currentUser.id}`, {
+            username,
+            email,
+            password,
+            avatar: avatar[0],
+         });
+         updateUser(res.data);
+         navigate('/profile');
+      } catch (err) {
+         console.log(err);
+         setError(err.response.data.message);
+      }
+   };
+
+   return (
+      <div className="profileUpdatePage">
+         <div className="formContainer">
+            <form onSubmit={handleSubmit}>
+               <h1>Update Profile</h1>
+               <div className="item">
+                  <label htmlFor="username">Username</label>
+                  <input
+                     id="username"
+                     name="username"
+                     type="text"
+                     defaultValue={currentUser.username}
+                  />
+               </div>
+               <div className="item">
+                  <label htmlFor="email">Email</label>
+                  <input
+                     id="email"
+                     name="email"
+                     type="email"
+                     defaultValue={currentUser.email}
+                  />
+               </div>
+               <div className="item">
+                  <label htmlFor="password">Password</label>
+                  <input id="password" name="password" type="password" />
+               </div>
+               <button>Update</button>
+               {error && <span>error</span>}
+            </form>
+         </div>
+         <div className="sideContainer">
+            <img
+               src={currentUser.avatar || '/noavatar.png'}
+               alt=""
+               className="avatar"
             />
-          </div>
-          <div className="item">
-            <label htmlFor="email">Email</label>
-            <input
-              id="email"
-              name="email"
-              type="email"
-            />
-          </div>
-          <div className="item">
-            <label htmlFor="password">Password</label>
-            <input id="password" name="password" type="password" />
-          </div>
-          <button>Update</button>
-        </form>
+         </div>
       </div>
-      <div className="sideContainer">
-        <img src="" alt="" className="avatar" />
-      </div>
-    </div>
-  );
+   );
 }
 
 export default ProfileUpdatePage;
