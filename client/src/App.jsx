bceabe007bec2c0dd802f9cd11c580eebcd99c62
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-13 19:47:39 +0200

    add:client/App.js: The loader for SingleApp has been added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './routes/login/login';
 import Register from './routes/register/register';
 import ProfileUpdatePage from './routes/profileUpdatePage/profileUpdatePage';
 import NewPostPage from './routes/newPostPage/newPostPage';
+import { singlePageLoader } from './lib/loaders';
 
 function App() {
    const router = createBrowserRouter([
@@ -26,6 +27,7 @@ function App() {
             {
                path: '/:id',
                element: <SinglePage />,
+               loader: singlePageLoader,
             },
 
             {
