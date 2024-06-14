9c1fc613a8641fa3515f1b87659a996eb1d5b666
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-14 13:34:00 +0200

    add:client/Filter.jsx: 01.The handleChange function has been created for getting data from inputs and transmitting it to the state 02.The handleChange function has been added for setting query into setSearchParams state

--- client/src/components/filter/Filter.jsx
+++ client/src/components/filter/Filter.jsx
@@ -1,74 +1,117 @@
-import "./filter.scss";
+import { useSearchParams } from 'react-router-dom';
+import './filter.scss';
+import { useState } from 'react';
 
 function Filter() {
-  return (
-    <div className="filter">
-      <h1>
-        Search results for <b>London</b>
-      </h1>
-      <div className="top">
-        <div className="item">
-          <label htmlFor="city">Location</label>
-          <input
-            type="text"
-            id="city"
-            name="city"
-            placeholder="City Location"
-          />
-        </div>
+   const [searchParams, setSearchParams] = useSearchParams();
+   const [query, setQuery] = useState({
+      type: searchParams.get('type') || '',
+      city: searchParams.get('city') || '',
+      property: searchParams.get('property') || '',
+      minPrice: searchParams.get('minPrice') || 0,
+      maxPrice: searchParams.get('maxPrice') || 1000000,
+      bedroom: searchParams.get('bedroom') || 1,
+   });
+
+   const handleChange = (e) => {
+      setQuery({
+         ...query,
+         [e.target.name]: e.target.value,
+      });
+   };
+
+   const handleFilter = () => {
+      setSearchParams(query);
+   };
+
+   return (
+      <div className="filter">
+         <h1>
+            Search results for <b>{searchParams.get('city')}</b>
+         </h1>
+         <div className="top">
+            <div className="item">
+               <label htmlFor="city" onChange={handleChange}>
+                  Location
+               </label>
+               <input
+                  type="text"
+                  id="city"
+                  name="city"
+                  placeholder="City Location"
+                  onChange={handleChange}
+                  defaultValue={query.city}
+               />
+            </div>
+         </div>
+         <div className="bottom">
+            <div className="item">
+               <label htmlFor="type">Type</label>
+               <select
+                  name="type"
+                  id="type"
+                  onChange={handleChange}
+                  defaultValue={query.type}
+               >
+                  <option value="">any</option>
+                  <option value="buy">Buy</option>
+                  <option value="rent">Rent</option>
+               </select>
+            </div>
+            <div className="item">
+               <label htmlFor="property">Property</label>
+               <select
+                  name="property"
+                  id="property"
+                  onChange={handleChange}
+                  defaultValue={query.property}
+               >
+                  <option value="">any</option>
+                  <option value="apartment">Apartment</option>
+                  <option value="house">House</option>
+                  <option value="condo">Condo</option>
+                  <option value="land">Land</option>
+               </select>
+            </div>
+            <div className="item">
+               <label htmlFor="minPrice">Min Price</label>
+               <input
+                  type="number"
+                  id="minPrice"
+                  name="minPrice"
+                  placeholder="any"
+                  onChange={handleChange}
+                  defaultValue={query.minPrice}
+               />
+            </div>
+            <div className="item">
+               <label htmlFor="maxPrice">Max Price</label>
+               <input
+                  type="text"
+                  id="maxPrice"
+                  name="maxPrice"
+                  placeholder="any"
+                  onChange={handleChange}
+                  defaultValue={query.maxPrice}
+               />
+            </div>
+            <div className="item">
+               <label htmlFor="bedroom">Bedroom</label>
+               <input
+                  type="text"
+                  id="bedroom"
+                  name="bedroom"
+                  placeholder="any"
+                  onChange={handleChange}
+                  defaultValue={query.bedroom}
+               />
+            </div>
+            <button onClick={handleFilter}>
+               <img src="/search.png" alt="" />
+            </button>
+         </div>
       </div>
-      <div className="bottom">
-        <div className="item">
-          <label htmlFor="type">Type</label>
-          <select name="type" id="type">
-            <option value="">any</option>
-            <option value="buy">Buy</option>
-            <option value="rent">Rent</option>
-          </select>
-        </div>
-        <div className="item">
-          <label htmlFor="property">Property</label>
-          <select name="property" id="property">
-            <option value="">any</option>
-            <option value="apartment">Apartment</option>
-            <option value="house">House</option>
-            <option value="condo">Condo</option>
-            <option value="land">Land</option>
-          </select>
-        </div>
-        <div className="item">
-          <label htmlFor="minPrice">Min Price</label>
-          <input
-            type="number"
-            id="minPrice"
-            name="minPrice"
-            placeholder="any"
-          />
-        </div>
-        <div className="item">
-          <label htmlFor="maxPrice">Max Price</label>
-          <input
-            type="text"
-            id="maxPrice"
-            name="maxPrice"
-            placeholder="any"
-          />
-        </div>
-        <div className="item">
-          <label htmlFor="bedroom">Bedroom</label>
-          <input
-            type="text"
-            id="bedroom"
-            name="bedroom"
-            placeholder="any"
-          />
-        </div>
-        <button>
-          <img src="/search.png" alt="" />
-        </button>
-      </div>
-    </div>
-  );
+   );
 }
 
 export default Filter;
