5708d6bb2a5ede6b7006684958fe3a401822732b
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-14 15:07:15 +0200

    add:api/user.route.js: An additional the savePost route has been added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -5,6 +5,7 @@ import {
    getUser,
    updateUser,
    deleteUser,
+   savePost,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
@@ -14,5 +15,6 @@ router.get('/', getUsers);
 router.get('/:id', verifyToken, getUser);
 router.put('/:id', verifyToken, updateUser);
 router.delete('/:id', verifyToken, deleteUser);
+router.post('/save', verifyToken, savePost);
 
 export default router;
