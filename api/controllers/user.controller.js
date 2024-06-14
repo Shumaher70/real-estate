11ce35c0586f680d1a8b06b3825b8d043147d2e0
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-14 15:08:16 +0200

    add:api/user.controller.js: An additional savePost function with save/delete logic has been added

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -13,6 +13,7 @@ export const getUsers = async (req, res) => {
 };
 export const getUser = async (req, res) => {
    const id = req.params.id;
+
    try {
       const user = await prisma.user.findUnique({
          where: {
@@ -79,5 +80,43 @@ export const deleteUser = async (req, res) => {
    } catch (error) {
       console.log('[deleteUser]', error);
       res.status(500).json({ message: 'Failed to delete user!' });
+   }
+};
+
+export const savePost = async (req, res) => {
+   const postId = req.body.postId;
+   const tokenUserId = req.userId;
+
+   try {
+      const savedPost = await prisma.savedPost.findUnique({
+         where: {
+            userId_postId: {
+               userId: tokenUserId,
+               postId,
+            },
+         },
+      });
+
+      if (savedPost) {
+         await prisma.savedPost.delete({
+            where: {
+               id: savedPost.id,
+            },
+         });
+         return res
+            .status(200)
+            .json({ message: 'Post removed from saved list' });
+      } else {
+         await prisma.savedPost.create({
+            data: {
+               userId: tokenUserId,
+               postId,
+            },
+         });
+         return res.status(200).json({ message: 'Post saved' });
+      }
+   } catch (error) {
+      console.log('[savePost]', error);
+      res.status(500).json({ message: 'Failed to save Post user!' });
    }
 };
