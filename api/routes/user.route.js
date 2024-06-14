d92a476336c7504570e4a6b75b6e20b0d08faa6e
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-14 19:34:36 +0200

    add:api/user.route.js: The route of profilePosts has been added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -6,15 +6,17 @@ import {
    updateUser,
    deleteUser,
    savePost,
+   profilePosts,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
 const router = express.Router();
 
 router.get('/', getUsers);
-router.get('/:id', verifyToken, getUser);
+router.get('/search/:id', verifyToken, getUser);
 router.put('/:id', verifyToken, updateUser);
 router.delete('/:id', verifyToken, deleteUser);
 router.post('/save', verifyToken, savePost);
+router.get('/profilePosts', verifyToken, profilePosts);
 
 export default router;
