4a689ca3fbe12395240631fd1e82aa9b5c66dd1e
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-14 19:32:13 +0200

    add:client/profilePage.jsx: The useLoaderHas been added

--- client/src/routes/profilePage/profilePage.jsx
+++ client/src/routes/profilePage/profilePage.jsx
@@ -1,5 +1,5 @@
-import { useContext } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { Suspense, useContext } from 'react';
+import { Await, Link, useLoaderData, useNavigate } from 'react-router-dom';
 
 import apiRequest from '../../lib/apiRequest';
 import Chat from '../../components/chat/Chat';
@@ -8,6 +8,8 @@ import './profilePage.scss';
 import { AuthContext } from '../../context/AuthContext';
 
 function ProfilePage() {
+   const data = useLoaderData();
+
    const { currentUser, updateUser } = useContext(AuthContext);
    const { username, email, avatar } = currentUser;
 
@@ -53,11 +55,30 @@ function ProfilePage() {
                      <button>Create New Post</button>
                   </Link>
                </div>
-               <List />
+               <Suspense fallback={<p>Loading...</p>}>
+                  <Await
+                     resolve={data.postResponse}
+                     errorElement={<p>Error loading posts!</p>}
+                  >
+                     {(postResponse) => (
+                        <List posts={postResponse.data.userPosts} />
+                     )}
+                  </Await>
+               </Suspense>
+
                <div className="title">
                   <h1>Saved List</h1>
                </div>
-               <List />
+               <Suspense fallback={<p>Loading...</p>}>
+                  <Await
+                     resolve={data.postResponse}
+                     errorElement={<p>Error loading posts!</p>}
+                  >
+                     {(postResponse) => (
+                        <List posts={postResponse.data.savedPosts} />
+                     )}
+                  </Await>
+               </Suspense>
             </div>
          </div>
          <div className="chatContainer">
