cda38ecab2ebdd3cc5ca5d037d5ab1b03c9d23ed
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-24 17:48:01 +0200

    add:client/Chat.jsx: 01.The dynamic data from server has been added instead of dummy 02.The handleOpenChat function has been added with logic for getting data for the certain chat and put it to setChat state has been added 03.The handleSubmit function has been added for getting formData from the form component and put it such text to post method to server and also put it to setChat  state for displaying it to the screen

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,123 +1,109 @@
-import { useState } from "react";
-import "./chat.scss";
+import { useContext, useState } from 'react';
+import { format } from 'timeago.js';
 
-function Chat() {
-  const [chat, setChat] = useState(true);
-  return (
-    <div className="chat">
-      <div className="messages">
-        <h1>Messages</h1>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
-        <div className="message">
-          <img
-            src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-            alt=""
-          />
-          <span>John Doe</span>
-          <p>Lorem ipsum dolor sit amet...</p>
-        </div>
+import { AuthContext } from '../../context/AuthContext';
+import apiRequest from '../../lib/apiRequest';
+
+import './chat.scss';
+
+function Chat({ chats }) {
+   const [chat, setChat] = useState(null);
+   const { currentUser } = useContext(AuthContext);
+
+   const handleOpenChat = async (id, receiver) => {
+      try {
+         const res = await apiRequest('/chats/' + id);
+
+         setChat({ ...res.data, receiver });
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      const formData = new FormData(e.target);
+      const text = formData.get('text');
+      if (!text) return;
+
+      try {
+         const res = await apiRequest.post('/messages/' + chat.id, { text });
+         setChat((prev) => ({
+            ...prev,
+            messages: [...prev.messages, res.data],
+         }));
+         e.target.reset();
+      } catch (error) {
+         console.log(error);
+      }
+   };
+
+   return (
+      <div className="chat">
+         <div className="messages">
+            <h1>Messages</h1>
+
+            {chats.map((chat) => (
+               <div
+                  className="message"
+                  key={chat.id}
+                  style={{
+                     backgroundColor: chat.seenBy.includes(currentUser.id)
+                        ? 'white'
+                        : '#fecd514e',
+                  }}
+                  onClick={() => handleOpenChat(chat.id, chat.receiver)}
+               >
+                  <img src={chat.receiver.avatar || '/noavatar.png'} alt="" />
+                  <span>{chat.receiver.username}</span>
+                  <p>{chat.lastMessage}</p>
+               </div>
+            ))}
+         </div>
+         {chat && (
+            <div className="chatBox">
+               <div className="top">
+                  <div className="user">
+                     <img
+                        src={chat.receiver.avatar || '/noavatar.png'}
+                        alt=""
+                     />
+                     {chat.receiver.username}
+                  </div>
+                  <span className="close" onClick={() => setChat(null)}>
+                     X
+                  </span>
+               </div>
+               <div className="center">
+                  {chat.messages.map((message) => (
+                     <div
+                        className="chatMessage"
+                        key={message.id}
+                        style={{
+                           alignSelf:
+                              message.userId === currentUser.id
+                                 ? 'flex-end'
+                                 : 'flex-center',
+                           textAlign:
+                              message.userId === currentUser.id
+                                 ? 'right'
+                                 : 'left',
+                        }}
+                     >
+                        <p>{message.text}</p>
+                        <span>{format(message.createdAt)}</span>
+                     </div>
+                  ))}
+               </div>
+               <form onSubmit={handleSubmit} className="bottom">
+                  <textarea name="text"></textarea>
+                  <button>Send</button>
+               </form>
+            </div>
+         )}
       </div>
-      {chat && (
-        <div className="chatBox">
-          <div className="top">
-            <div className="user">
-              <img
-                src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                alt=""
-              />
-              John Doe
-            </div>
-            <span className="close" onClick={()=>setChat(null)}>X</span>
-          </div>
-          <div className="center">
-            <div className="chatMessage">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-            <div className="chatMessage own">
-              <p>Lorem ipsum dolor sit amet</p>
-              <span>1 hour ago</span>
-            </div>
-          </div>
-          <div className="bottom">
-            <textarea></textarea>
-            <button>Send</button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
+   );
 }
 
 export default Chat;
