38f56eed25a398f08eb1e1fc0d0ce76c32b3201e
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-24 20:58:21 +0200

    add:api/user.route.js: Additional route for notification has been added

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,6 +7,7 @@ import {
    deleteUser,
    savePost,
    profilePosts,
+   getNotificationNumber,
 } from '../controllers/user.controller.js';
 import { verifyToken } from '../middleware/verifyToken.js';
 
@@ -18,5 +19,6 @@ router.put('/:id', verifyToken, updateUser);
 router.delete('/:id', verifyToken, deleteUser);
 router.post('/save', verifyToken, savePost);
 router.get('/profilePosts', verifyToken, profilePosts);
+router.get('/notification', verifyToken, getNotificationNumber);
 
 export default router;
