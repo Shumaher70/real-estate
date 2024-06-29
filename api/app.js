56a7c6ba92adc66e6bda4e4854d74c3faed44c9f
Oleg Vrabiy <olegvraby@gmail.com>
2024-06-29 15:41:14 +0200

    server finish

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import dotenv from 'dotenv';
+import path from 'path';
 
 import authRoute from './routes/auth.route.js';
 import postRoute from './routes/post.route.js';
@@ -8,14 +10,19 @@ import userRoute from './routes/user.route.js';
 import chatRoute from './routes/chat.route.js';
 import messageRoute from './routes/message.route.js';
 
+dotenv.config();
+
+const __dirname = path.resolve();
 const app = express();
 
 app.use(
    cors({
       origin: process.env.CLIENT_URL,
       credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
    })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -25,6 +32,12 @@ app.use('/api/users', userRoute);
 app.use('/api/chats', chatRoute);
 app.use('/api/messages', messageRoute);
 
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
 app.listen(8800, () => {
-   console.log('server is running');
+   console.log('Сервер запущен на порту 8800');
 });
